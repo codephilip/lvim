f1c74c8b57fc3fcbf07550c50a68d6aa21556586
codephilip <philip@meiers.in>
2024-02-02 23:36:26 -0800

    autocomplete lsp

--- config.lua
+++ config.lua
@@ -38,15 +38,65 @@ lvim.plugins = {
       { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
       { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
     },
-    -- Bufferline
-    {
-      'akinsho/bufferline.nvim',
-      dependencies = 'nvim-tree/nvim-web-devicons'
-    },
 
     -- Colorscheme
     {
       'folke/tokyonight.nvim',
+    },
+    {
+      "goolord/alpha-nvim",
+      lazy = true,
+    },
+    {
+      "phaazon/hop.nvim",
+      lazy = true,
+    },
+
+    -- Language Support
+    -- Added this plugin.
+    {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v1.x',
+      dependencies = {
+        -- LSP Support
+        { 'neovim/nvim-lspconfig' },             -- Required
+        { 'williamboman/mason.nvim' },           -- Optional
+        { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+
+        -- Autocompletion
+        { 'hrsh7th/nvim-cmp' },         -- Required
+        { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+        { 'hrsh7th/cmp-buffer' },       -- Optional
+        { 'hrsh7th/cmp-path' },         -- Optional
+        { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+        { 'hrsh7th/cmp-nvim-lua' },     -- Optional
+
+        -- Snippets
+        { 'L3MON4D3/LuaSnip' },             -- Required
+        { 'rafamadriz/friendly-snippets' }, -- Optional
+      }
+    },
+
+
+    -- Telescope (Fuzzy Finder)
+    {
+      'nvim-telescope/telescope.nvim',
+      lazy = true,
+      dependencies = {
+        { 'nvim-lua/plenary.nvim' },
+      }
+    },
+
+
+    -- Treesitter
+    {
+      "nvim-treesitter/nvim-treesitter",
+    },
+
+    -- Which-key
+    {
+      'folke/which-key.nvim',
+      lazy = true,
     },
   },
 }
